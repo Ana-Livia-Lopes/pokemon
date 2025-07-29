a1500e1740e4b23563bd74f255cc1fac853701c3
Isadora-Gomes <isasilvacpv@gmail.com>
2025-07-29 08:59:18 -0300

    conexao

---
+++ conexao.php
@@ -0,0 +1,18 @@
+<?php
+    $dbname = 'db_pokemon';
+    $hostname = 'localhost';
+    $password = '';
+    $username = 'root';
+
+    $conexao = new mysqli($hostname, $username,$password, $dbname);
+
+    if ($conexao->connect_error) {
+        die("falha na coneção: ". $conexao->connect_error);
+    };
+
+    $sql_pokemons= "SELECT nome_pokemon, tipo_pokemon, loc_pokemon, data_pokemon, hp_pokemon, ataque_pokemon, defesa_pokemon, obs_pokemon 
+    FROM cadastrar_pokemon
+    ORDER BY nome_pokemon ASC";
+    
+    $resultado_pokemons = $conexao -> query($sql_pokemons);
+?>
